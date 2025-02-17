bca81d386b78f955699d6582ee014d2507ed451d
Web Developer Sivasundararaj <siva.avis47@gmail.com>
2025-02-17 14:55:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+var app = angular.module('myapp',[]);
+
+
+app.controller('myctrl', function($scope){
+$scope.img_source = "https://angularjs.org/img/AngularJS-large.png";
+$scope.first="Home";
+$scope.sec="About";
+$scope.third="Service";
+$scope.four="Project";
+$scope.cv="CV";
+$scope.five="Contact";
+
+$scope.bannerimg="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?t=st=1723893418~exp=1723897018~hmac=b4af993f01fa6432e67b01da8aefb4f21ae168304cbe5997c4dfb94048a69a3a&w=740";
+$scope.ttit = "Hello Guy's";
+$scope.bsubtit = "I'm CSSR Frontend Web Developer";
+$scope.tstit = "Base in India";
+
+ $scope.uiuxtitle = "UI/UX Design";
+$scope.uiuxdes = "We create intuitive and visually stunning user interfaces and experiences for both web and mobile applications. Our designs are focused on enhancing user satisfaction and driving engagement.";
+
+$scope.wmtitle="Web and Mobile App Development";
+$scope.wmdes="From concept to launch, we build robust and scalable web and mobile applications tailored to meet your business needs and goals.";
+
+$scope.seotit ="SEO & Digital Marketing";
+$scope.seodes = "Enhance your online presence with our expert SEO strategies and digital marketing solutions. We optimize your website for search engines and drive targeted traffic to help you achieve your business objectives.";
+
+$scope.logotit = "Logo Design";
+$scope.logodes = "Our logo design service provides you with a unique and memorable visual identity that effectively communicates your brand’s essence and values.";
+
+$scope.brandtit="Brand Design";
+$scope.branddes="Build a cohesive and memorable brand with our comprehensive brand design services. We help you define your brand’s voice, visual identity, and positioning to ensure consistency across all touchpoints."
+
+$scope.share="Let's share your ideas to collaborate!";
+
+$scope.side="Sidebar Menu collapse & Expand";
+$scope.bookmark="Bookmark Info Card Design";
+$scope.valid="Form Validation Javascript";
+$scope.social="Social Icon Name Hover Effect";
+
+$scope.figma ="https://img.icons8.com/fluency/48/figma.png"
+$scope.photoshop= "https://img.icons8.com/3d-fluency/94/adobe-photoshop.png";
+$scope.angularjs ="https://img.icons8.com/color/48/angularjs.png";
+$scope.javascript="https://img.icons8.com/fluency/48/javascript.png";
+$scope.css="https://img.icons8.com/fluency/48/css3.png";
+$scope.html="https://img.icons8.com/color/48/html-5.png";
+$scope.wordpress="https://img.icons8.com/color/48/wordpress.png"
+$scope.corel="https://img.icons8.com/external-others-inmotus-design/67/external-Corel-Draw-applications-and-programs-others-inmotus-design.png"
+$scope.adobexd="https://img.icons8.com/color/48/adobe-xd--v1.png";
+
+$scope.uiux="https://img.icons8.com/carbon-copy/100/29b6e3/web-design.png";
+$scope.mob="https://img.icons8.com/external-flatart-icons-outline-flatarticons/64/29b6e3/external-mobile-app-ux-and-ui-flatart-icons-outline-flatarticons.png";
+$scope.seo="https://img.icons8.com/carbon-copy/100/29b6e3/positive-dynamic--v1.png";
+$scope.logo="https://img.icons8.com/dotty/80/29b6e3/design.png";
+$scope.brnad="https://img.icons8.com/laces/64/29b6e3/tag.png";
+
+/* $scope.email="https://img.icons8.com/parakeet-line/38/new-post.png";
+ */
+ $scope.email="https://img.icons8.com/color/48/gmail-new.png";
+ $scope.whatsup="https://img.icons8.com/color/48/whatsapp--v1.png";
+
+$scope.pro="https://img.icons8.com/dotty/80/29b6e3/easy.png";
+$scope.sidebarmenu="https://imgur.com/XjkJMFV.png";
+
+});
+
